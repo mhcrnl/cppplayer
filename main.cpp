5e9fcda94b18b136e8ae0c9b975ac5d19fed447f
david <david@debian>
2016-01-09 15:26:27 +0100

    Add daemon/client support

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include <vector>
 #include <thread>
 #include <condition_variable>
+#include <cstdio>
+#include <fcntl.h>
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 
 #include "song.h"
@@ -37,10 +41,21 @@ int main(int argc, char* argv[])
 	vector<path> songList;
 	if(argc>1) {
 		//FIXME
-		if(argc>2) {daemonize(); daemon=true;}
+		if(argc>2 && *argv[2] == 'd') {
+			daemon=true;
+			daemonize(); 
+		}
 		path p(argv[1]);
-		if(is_directory(p))
+		if(is_directory(p)) {
+			if(daemon)
+				mkfifo("/tmp/player++", 0666);
 			getSongList(p, songList);
+		} else {
+			int fd = open("/tmp/player++", O_WRONLY);
+			write(fd, argv[1], 1);
+			close(fd);
+			return 0;
+		}
 	} else {
 		getSongList(".", songList);
 	}
@@ -51,25 +66,40 @@ int main(int argc, char* argv[])
 
 	Song* song = new Song;
 	thread mplayer(PlayList, song, songList);
-	if(!daemon)
-	while(!song->isStop()) {
-		char c = getch();
-		if(c == 'q' || c == 'Q') {
-			cv.notify_one();
-			song->setStop();
-		} else if(c == 'n' || c == 'N') {
-			cv.notify_one();
-			song->setNext();
-		} else if(c == 'p' || c == 'P') {
-			if(song->isPause()) {
+	if(!daemon) {
+		while(!song->isStop()) {
+			char c = getch();
+			if(c == 'q' || c == 'Q') {
+				song->setStop();
+				cv.notify_one();
+			} else if(c == 'n' || c == 'N') {
+				song->setNext();
+				cv.notify_one();
+			} else if(c == 'p' || c == 'P') {
 				song->setPause();
 				cv.notify_one();
-			} else {
+			}
+		}
+	} else {
+		while(!song->isStop()) {
+			int fd = open("/tmp/player++", O_RDONLY);
+			char c = 0;
+			read(fd, &c, 1);
+			close(fd);
+			if(c == 'q' || c == 'Q') {
+				song->setStop();
 				cv.notify_one();
+			} else if(c == 'n' || c == 'N') {
+				song->setNext();
+				cv.notify_one();
+			} else if(c == 'p' || c == 'P') {
 				song->setPause();
+				cv.notify_one();
 			}
 		}
 	}
+	unlink("/tmp/player++");
+
 	mplayer.join();
 	delete song;
 }
