8ca7f2983d0dd2e289ec2a0553ea3527b12b1196
stakewinner00 <stakewinner00@mykolab.com>
2016-04-24 20:17:58 +0200

    create constructor/destructor of manager class.

--- manager.cpp
+++ manager.cpp
@@ -1,5 +1,19 @@
 #include "manager.h"
 
+Manager::Manager() {
+	//Delete pipes, if exist (the program exit abnormaly)
+	unlink(conf.GetDaemonPipe().c_str()); 
+	unlink(conf.GetClientPipe().c_str());
+
+	mkfifo(conf.GetDaemonPipe().c_str(), 0666);
+	mkfifo(conf.GetClientPipe().c_str(), 0666);
+}
+
+Manager::~Manager() {
+	unlink(conf.GetDaemonPipe().c_str()); 
+	unlink(conf.GetClientPipe().c_str());
+}
+
 void Manager::StartServer() {
 
 }
--- manager.h
+++ manager.h
@@ -5,6 +5,10 @@
 //and controls the execution of the music
 class Manager {
 public:
+	//Create/destroy pipes for the client-server comunication
+	Manager();
+	~Manager();
+
 	//Start the main loop
 	void StartServer();
 private:
