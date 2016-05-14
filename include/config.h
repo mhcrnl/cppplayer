3b38d0bfca9cf0185cac15aca38e8fe5a8a1ad07
stakewinner00 <stakewinner00@mykolab.com>
2016-05-14 19:21:18 +0200

    allow relative paths

--- include/config.h
+++ include/config.h
@@ -12,9 +12,12 @@ public:
 	std::string GetDaemonPipe() const;
 	std::string GetClientPipe() const;
 	std::string GetPidFile()	const;
+	std::string GetDbFile()		const;
 	path GetDir() const;
 	bool GetAutostart() const;
 private:
+	std::string MakeAbsolute(const std::string file);
+	std::string GetHome();
 	struct Options {
 		//Pipe used by the daemon to write to client
 		std::string daemonpipe = "/tmp/dplayer++";
@@ -25,6 +28,10 @@ private:
 		//File to store the pid number so we can check if the daemon is really running
 		std::string pidfile = "/tmp/player++.pid";
 
+		//Database file
+		std::string dbfile	= "~/.config/player++/db.sql";
+
+		//Location of the songs
 		path dir = ".";
 
 		//If true, change automatically Status::Stoped to Status::Playing
--- src/config.cpp
+++ src/config.cpp
@@ -9,33 +9,37 @@ using namespace boost::program_options;
 
 
 void Config::Load() {
-	std::string home = getenv("HOME");
-	if(home.empty()) {
-		throw std::runtime_error("HOME env variable not found, exiting");
-	}
+  #ifdef DEBUG
+    std::cout << "Using config file " << MakeAbsolute("~/.config/player++/daemon.conf") << std::endl;
+  #endif
 
-	std::ifstream config(home+"/.config/player++");
+	std::ifstream config(MakeAbsolute("~/.config/player++/daemon.conf"));
   if(!config.is_open()) {
-    std::cerr << "Config file could not be open, using default values" << std::endl;
-    return;
-  }
-
-  options_description desc("Options");
-  desc.add_options()
+        std::cerr << "Config file could not be open, using default values" << std::endl;
+  } else {
+    options_description desc("Options");
+    desc.add_options()
       ("daemon_pipe", value<std::string>(&opt.daemonpipe))
       ("client_pipe", value<std::string>(&opt.clientpipe))
       ("pid_file",    value<std::string>(&opt.pidfile))
+      ("db_file",     value<std::string>(&opt.dbfile))
       ("music_folder", value<path>(&opt.dir))
       ("auto_start", value<bool>(&opt.autostart))
+
     ;
 
+    variables_map vm = variables_map();
+    store(parse_config_file(config , desc, true), vm);
+    notify(vm); 
 
-  variables_map vm = variables_map();
-  store(parse_config_file(config , desc, true), vm);
-  notify(vm); 
-    //Load config file
+    config.close();
+  }
 
-  config.close();
+  opt.daemonpipe  =MakeAbsolute(opt.daemonpipe);
+  opt.clientpipe  =MakeAbsolute(opt.clientpipe);
+  opt.pidfile     =MakeAbsolute(opt.pidfile);
+  opt.dbfile      =MakeAbsolute(opt.dbfile);
+  opt.dir         =MakeAbsolute(opt.dir.c_str());
 }
 
 std::string Config::GetDaemonPipe() const {
@@ -50,10 +54,34 @@ std::string Config::GetPidFile() const {
   return opt.pidfile;
 }
 
+std::string Config::GetDbFile() const {
+  return opt.dbfile;
+}
+
 path Config::GetDir() const {
   return opt.dir;
 }
 
 bool Config::GetAutostart() const {
   return opt.autostart;
+}
+
+//Private functions
+
+std::string Config::MakeAbsolute(std::string file) {
+  auto pos = file.find('~');
+  if(pos != std::string::npos) {
+    file.erase(pos,1);
+    file.insert(pos, GetHome());
+  }
+  
+  return file;
+}
+
+std::string Config::GetHome() {
+  static std::string home = getenv("HOME");
+  if(home.empty()) {
+    throw std::runtime_error("HOME env variable not found, exiting");
+  }
+  return home;
 }
